28e1761e32d32dc496aa42287f0b4ca051868884
abdelrahman-kamel-elgendy <abdelrahman.kamel.elgendy@gmail.com>
2026-05-03 19:27:32 +0300

    created get user by email endpoint

--- Assignment7/controllers/user.controller.js
+++ Assignment7/controllers/user.controller.js
@@ -46,4 +46,18 @@ const login = async (req, res) => {
     }
 };
 
-module.exports = { signup, login };
+const getUserByEmail = async (req, res) => {
+    try {
+        const { email } = req.query;
+
+        const user = await User.findOne({ where: { email } });
+        if (!user)
+            return res.status(404).json({ message: "no user found" });
+
+        res.status(200).json({ user });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports = { signup, login, getUserByEmail };
--- Assignment7/routes/user.routes.js
+++ Assignment7/routes/user.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { signup, login } = require("../controllers/user.controller");
+const { signup, login, getUserByEmail } = require("../controllers/user.controller");
 
 module.exports = express.Router()
     .post("/signup", signup)
     .post("/login", login)
+    .get("/by-email", getUserByEmail)
